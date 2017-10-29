0660798d4c1bcbd077ecb0c1e85cb75f42855d52
Shreyas <shrsgraj007@gmail.com>
2017-10-28 21:19:09 -0700

    Done with functional requirement,need to implement reload

--- lab4.js
+++ lab4.js
@@ -6,18 +6,19 @@ var room_shuffle = shuffle(rooms);
 var weapon_shuffle = shuffle(weapons);
 var suspect_shuffle = shuffle(suspects);
 var domstorage = window.localStorage;
-var today = new Date().toJSON().slice(0, 10).replace(/-/g, '/');
 if (!domstorage.losses) {
     domstorage.losses = 0;
 }
 if (!domstorage.wins) {
     domstorage.wins = 0;
 }
-local_storage();
 var secretkey = {};
 var user_card = [];
 var computer_card = [];
 var user_name;
+var common_card = [];
+var database = [];
+
 
 function welcomeName() {
     user_name = document.getElementById("welcome")[0].value;
@@ -35,6 +36,22 @@ function welcomeAgain() {
     modify_choice();
 }
 
+function today() {
+    var current = new Date();
+    var yyyy = current.getFullYear();
+    var month = current.getMonth() + 1;
+    var date = current.getDate();
+
+    if (month < 10) {
+        month = '0' + month;
+    }
+    if (date < 10) {
+        date = '0' + date;
+    }
+    return date + '/' + month + '/' + yyyy;
+
+}
+
 function checkForWin() {
     var e = document.getElementById('rooms');
     var strRooms = e.options[e.selectedIndex].value;
@@ -43,7 +60,7 @@ function checkForWin() {
     var e = document.getElementById('weapons');
     var strWeapons = e.options[e.selectedIndex].value;
     if (strRooms === secretkey.rooms && strWeapons === secretkey.weapons && strSuspects === secretkey.suspects) {
-        user_guess = "You guessed \"" + strSuspects + " in the " + strRooms +
+        user_guess = user_name+" guessed \"" + strSuspects + " in the " + strRooms +
             " with a " + strWeapons + "\"<br/>";
         call_session(user_guess);
         if (domstorage.losses) {
@@ -54,11 +71,11 @@ function checkForWin() {
 
         document.getElementById('compute').innerHTML = "That was the correct guess! " + strSuspects +
             " did it with the " + strWeapons + " in the " + strRooms + "!</br>Click to start a new game:";
-        document.getElementById('compute').innerHTML += "<input type=\"button\" value=\"New Game\" onclick=\"window.location.reload()\"/>";
+        document.getElementById('compute').innerHTML += "<input type=\"button\" value=\"New Game\" onclick=\"refresh()\"/>";
     }
     else {
         // var matched_cards = element_exists(strRooms,strWeapons,strSuspects);
-        var user_guess = "You guessed \"" + strSuspects + " in the " + strRooms +
+        var user_guess = user_name+" guessed \"" + strSuspects + " in the " + strRooms +
             " with a " + strWeapons + "\"<br/>";
         call_session(user_guess);
         document.getElementById('compute').innerHTML = "Sorry that was an incorrect guess!";
@@ -74,24 +91,105 @@ function checkForWin() {
 
 }
 
+function refresh() {
+    sessionStorage.clear();
+    var template = "<p>\n" +
+        "    Rooms: Kitchen, Study, Living Room, Dining Room, Library, Lounge, Billiards Room, Hall, Ball Room<br/>\n" +
+        "    Guests: Mrs. Peacock, Mrs. Green, Miss Scarlet, Colonel Mustard, Professor Plum, Mrs White<br/>\n" +
+        "    Weapons: Pistol, Knife, Wrench, Lead Pipe, Candlestick, Revolver <br/>\n" +
+        "</p>\n" +
+        "\n" +
+        "<p>\n" +
+        "<FORM id=\"welcome\" onsubmit=\"return emptyFieldCheck()\">\n" +
+        "    Name:\n" +
+        "    <input type=\"text\" name = \"user_name\" autofocus=\"true\" placeholder=\"name_here\" required=\"required\"/>\n" +
+        "    <input type=\"button\" value=\"Enter\" onclick=\"welcomeName()\"/>\n" +
+        "</FORM>\n" +
+        "<p id=\"select_room\">\n" +
+        "\n" +
+        "<FORM>\n" +
+        "    <label>Guess Room:</label>\n" +
+        "    <select name=\"rooms\" id = \"rooms\">\n" +
+        "        <option value=\"Kitchen\">Kitchen</option>\n" +
+        "        <option value=\"Study\">Study</option>\n" +
+        "        <option value=\"Living Room\">Living Room</option>\n" +
+        "        <option value=\"Dining Room\">Dining Room</option>\n" +
+        "        <option value=\"Lounge\">Lounge</option>\n" +
+        "        <option value=\"Billiards Room\">Billiards Room</option>\n" +
+        "        <option value=\"Hall\">Hall</option>\n" +
+        "        <option value=\"Ball Room\">Ball Room</option>\n" +
+        "        <option value=\"Library\">Library</option>\n" +
+        "    </select>\n" +
+        "\n" +
+        "    <br/>\n" +
+        "    <label>Guess Suspect:</label>\n" +
+        "    <select name=\"suspects\" id = \"suspects\">\n" +
+        "        <option value=\"Mrs. Peacock\">Mrs. Peacock</option>\n" +
+        "        <option value=\"Mrs. Green\">Mrs. Green</option>\n" +
+        "        <option value=\"Miss Scarlet\">Miss Scarlet</option>\n" +
+        "        <option value=\"Colonel Mustard\">Colonel Mustard</option>\n" +
+        "        <option value=\"Professor Plum\">Professor Plum</option>\n" +
+        "        <option value=\"Mrs. White\">Mrs. White</option>\n" +
+        "    </select>\n" +
+        "    <br/>\n" +
+        "    <label>Guess Weapon:</label>\n" +
+        "    <select name=\"weapons\" id = \"weapons\">\n" +
+        "        <option value=\"Pistol\">Pistol</option>\n" +
+        "        <option value=\"Knife\">Knife</option>\n" +
+        "        <option value=\"Wrench\">Wrench</option>\n" +
+        "        <option value=\"Lead Pipe\">Lead Pipe</option>\n" +
+        "        <option value=\"Candlestick\">Candlestick</option>\n" +
+        "        <option value=\"Revolver\">Revolver</option>\n" +
+        "    </select>\n" +
+        "    <br/>\n" +
+        "    <input type=\"button\" id = \"guess\" disabled=\"true\" value=\"Guess\" onclick=\"checkForWin()\"/>\n" +
+        "</FORM>\n" +
+        "<p id ='compute'>\n" +
+        "\n" +
+        "</p>\n" +
+        "<input type=\"button\" id =\"history button\" value = \"Hide History\" onclick=\"show_history()\"><br/>\n" +
+        "<div id=\"history\">\n" +
+        "\n" +
+        "</div>\n" +
+        "\n" +
+        "<input type=\"button\" id = \"record button\" value = \"Hide Record\" onclick=\"show_record()\"><br/>\n" +
+        "<div id=\"record\">\n" +
+        "\n" +
+        "</div>\n";
+    reset();
+    document.getElementById('render').innerHTML = template;
+    local_storage();
+}
+
+function reset() {
+    secretkey = {};
+    user_card = [];
+    computer_card = [];
+    user_name = undefined;
+    room_shuffle = shuffle(rooms);
+    weapon_shuffle = shuffle(weapons);
+    suspect_shuffle = shuffle(suspects);
+    common_card = randomize_room_cards(room_shuffle, weapon_shuffle, suspect_shuffle);
+    common_card = shuffle(common_card);
+    user_card = split_array(common_card, 0, Math.floor(common_card.length / 2));
+    computer_card = split_array(common_card, Math.floor(common_card.length / 2), common_card.length);
+    console.log(secretkey);
+    database = [];
+
+}
+
 function modify_choice() {
-    // console.log(user_card);
-    //console.log("rooms",rooms);
     var new_room = compare_string(rooms, user_card);
     new_room = shuffle(new_room);
     var new_suspect = compare_string(suspects, user_card);
     new_suspect = shuffle(new_suspect);
     var new_weapon = compare_string(weapons, user_card);
     new_weapon = shuffle(new_weapon);
-    // console.log(new_room);
-    // console.log(new_suspect);
-    // console.log(new_weapon);
     document.getElementById('rooms').innerHTML = '';
     document.getElementById('suspects').innerHTML = "";
     document.getElementById('weapons').innerHTML = "";
     for (var i = 0; i < new_room.length; i++) {
         document.getElementById('rooms').innerHTML += "<option value=\"" + new_room[i] + "\">" + new_room[i] + "</option>";
-        //console.log(new_room[i]);
     }
     for (var i = 0; i < new_suspect.length; i++) {
         document.getElementById('suspects').innerHTML += "<option value=\"" + new_suspect[i] + "\">" + new_suspect[i] + "</option><br />";
@@ -126,13 +224,10 @@ function compare_string(arr1, arr2) {
 function computer_turn() {
     var new_room = compare_string(rooms, computer_card);
     new_room = shuffle(new_room);
-    //console.log('new room',new_room);
     var new_suspect = compare_string(suspects, computer_card);
     new_suspect = shuffle(new_suspect);
-    //console.log('new suspect',new_suspect);
     var new_weapon = compare_string(weapons, computer_card);
     new_weapon = shuffle(new_weapon);
-    //console.log('new weapon',new_weapon);
     var room_chosen = new_room[Math.floor(Math.random() * new_room.length)];
     var suspect_chosen = new_suspect[Math.floor(Math.random() * new_suspect.length)];
     var weapon_chosen = new_weapon[Math.floor(Math.random() * new_weapon.length)];
@@ -142,7 +237,7 @@ function computer_turn() {
     document.getElementById('compute').innerHTML = comp_guess;
     if (room_chosen === secretkey.rooms && weapon_chosen === secretkey.weapons && suspect_chosen === secretkey.suspects) {
         document.getElementById('compute').innerHTML += "Computer made the correct guess! </br>Click to start a new game:";
-        document.getElementById('compute').innerHTML += "<input type=\"button\" value=\"New Game\" onclick=\"window.location.reload()\"/>";
+        document.getElementById('compute').innerHTML += "<input type=\"button\" value=\"New Game\" onclick=\"refresh()\"/>";
         if (domstorage.wins) {
             domstorage.wins = Number(domstorage.wins) + 1;
         }
@@ -151,8 +246,6 @@ function computer_turn() {
     else {
         document.getElementById('compute').innerHTML += "The Computer made an incorrect guess!";
         var incorrect_cards = element_exists(room_chosen, weapon_chosen, suspect_chosen, user_card);
-        // console.log(room_chosen,weapon_chosen, suspect_chosen, user_card);
-        //console.log('chosen card',incorrect_cards);
         var card = incorrect_cards[Math.floor(Math.random() * incorrect_cards.length)];
         document.getElementById('compute').innerHTML += " You holds the card for " + card + ".<br/>";
         document.getElementById('compute').innerHTML += "Click to continue:" +
@@ -188,15 +281,9 @@ function emptyFieldCheck() {
 
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
-
-    // While there remain elements to shuffle...
     while (0 !== currentIndex) {
-
-        // Pick a remaining element...
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
-
-        // And swap it with the current element.
         temporaryValue = array[currentIndex];
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
@@ -228,23 +315,15 @@ function show_record() {
     }
 }
 
-var database = [];
 
 function call_session(player_guess) {
 
     if (window.sessionStorage) {
         database.push(player_guess);
         sessionStorage.guess = database;
-        //console.log('true');
-        // }
-        // else{
-        //     sessionStorage.guess = "";
-        //     console.log('false');
     }
 
     document.getElementById('history').innerHTML = sessionStorage.guess.replace(/,/g, '');
-    //console.log('session',sessionStorage.guess);
-
 }
 
 function local_storage(player) {
@@ -259,12 +338,12 @@ function local_storage(player) {
             document.getElementById('record').innerHTML += names.k[i];
         }
         if (player === 'Computer') {
-            var temp_1 = "Computer vs  " + domstorage.user + "," + today + "  Computer won<br />";
+            var temp_1 = "Computer vs  " + domstorage.user + "," + today() + "  Computer won<br />";
             names.k.push(temp_1);
             document.getElementById('record').innerHTML += temp_1;
         }
         else if ((player === user_name) && (user_name !== undefined)) {
-            var temp_2 = "Computer vs  " + domstorage.user + ", " + today + "  " + domstorage.user + " won<br />";
+            var temp_2 = "Computer vs  " + domstorage.user + ", " + today() + "  " + domstorage.user + " won<br />";
             names.k.push(temp_2);
             document.getElementById('record').innerHTML += temp_2;
         }
@@ -302,7 +381,6 @@ function randomize_room_cards(room_card, weapon_card, suspect_card) {
         }
 
     }
-    //
     for (var i = 0; i < weapon_card.length; i++) {
         if (i === 0) {
             secretkey.weapons = weapon_card[i];
@@ -311,7 +389,6 @@ function randomize_room_cards(room_card, weapon_card, suspect_card) {
             common_cards[k++] = weapon_card[i];
         }
     }
-    //
     for (var i = 0; i < suspect_card.length; i++) {
         if (i === 0) {
             secretkey.suspects = suspect_card[i];
@@ -331,13 +408,4 @@ function split_array(arr, m, n) {
     }
     return ret;
 }
-
-var common_card = randomize_room_cards(room_shuffle, weapon_shuffle, suspect_shuffle);
-common_card = shuffle(common_card);
-user_card = split_array(common_card, 0, Math.floor(common_card.length / 2));
-computer_card = split_array(common_card, Math.floor(common_card.length / 2), common_card.length);
-console.log(secretkey);
-// console.log(common_card);
-// console.log("user card",user_card);
-// console.log("Comp card",computer_card);
 
